acc2df4ee77f53400fa06367a2a0a92e832d10dc
Sreejan09 <sreejangupta9@gmail.com>
2024-08-19 18:01:24 +0530

    First Commit on Task 1 Data set to store it in a dictionary

--- HAR/MakeDataset.py
+++ HAR/MakeDataset.py
@@ -77,7 +77,6 @@ y = np.concatenate((y_train,y_test))
 # split the data into training and testing sets. Change the seed value to obtain different random splits.
 seed = 4
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.3,random_state=seed,stratify=y)
-
 print("Training data shape: ",X_train.shape)
 print("Testing data shape: ",X_test.shape)
 
